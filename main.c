850b789ddd940d50c373559d26e43be65cdf5297
Daria Sova <todariasova@gmail.com>
2016-01-26 16:58:39 -0800

    Fixed string copy

--- main.c
+++ main.c
@@ -52,17 +52,23 @@ char* get_cmd()
 
 int main ( void )
 {
+  int MAX_JOBS_NUMBER=5;
+  struct bg_job
+  {
+    int pid;
+    int terminated;
+    char *command;
+  };
 
-    struct bg_job
-    {
-      int pid;
-      char *command;
-    };
+  typedef struct bg_job BG_JOB;
+  BG_JOB jobs_list[MAX_JOBS_NUMBER];
+  //counter for gb jobs
+  int bgj_counter = 0;
 
-    typedef struct bg_job BG_JOB;
-    BG_JOB jobs_list[5];
-    //counter for gb jobs
-    int bgj_counter = 0;
+  for(int i=0; i<MAX_JOBS_NUMBER; i++)
+  {
+    jobs_list[i].command=NULL;
+  }
 
   for (;;)
   {
@@ -74,18 +80,26 @@ int main ( void )
     {
       printf ("\nChild is DONE");
       printf("[proc %d exited with code %d]\n", pid, WEXITSTATUS(status));
+      if(pid>0)
+      {
+        int i=0;
+        while(jobs_list[i].pid!=pid)
+        {
+          i++;
+        }
+        jobs_list[i].command=NULL;
+        bgj_counter--;
+      }
     }
-    printf ("\nStatus pid %d", waitpid(-1, &status, WNOHANG));
+
+    //printf ("\nStatus pid %d", waitpid(-1, &status, WNOHANG));
     //printf ("\nPID is %d\n",pid);
     //check_ch_process();
     char *cmd = get_cmd();
-
-
     printf ("Got: [%s]\n", cmd);
 
     char** args = calloc(100, sizeof(char*));
     parseInputString(cmd, args, " \n");
-
 
     if(strcmp(args[0], "cd")==0)
     {
@@ -93,70 +107,73 @@ int main ( void )
     }
     else if(strcmp(args[0], "bglist")==0)
     {
-      for(int i=0; i<5; i++)
+      //print current bg jobs
+      for(int i=0; i<MAX_JOBS_NUMBER; i++)
       {
         if(jobs_list[i].command!=NULL)
         {
           printf("%d:\t%s\n", i, jobs_list[i].command);
         }
       }
+      printf("Total Background jobs: %d\n", bgj_counter);
     }
     else {
-
-      int pid= fork();              //fork child
-
-      if(pid==0){               //Child
-        if(strcmp(args[0], "bg")==0)
-        {
-          execvp(args[1], &args[1]);
-          exit(1);
-        }
-        else
-        {
-          execvp(args[0], args);
-          printf( "Child process could not do execvp\n");
-          exit(1);
-        }
-      } else if (pid<0){
-        printf( "Forking failed...\n");
-      }
-
-
-
-      else {                    //Parent
-
-        if(strcmp(args[0], "bg")!=0)
-        {
-          printf("[proc %d started]\n", pid);
-          waitpid(pid, NULL, 0);
-        }
-        else
-        {
-          //check the limit [5]
-          if(bgj_counter<4)
+      printf("Forking!!!!");
+      int ch_pid= fork();              //fork child
+      if(ch_pid >=0)
+      {
+        if(ch_pid==0)
+        {               //Child
+          if(strcmp(args[0], "bg")==0)
           {
-            jobs_list[bgj_counter].pid = pid;
-            jobs_list[bgj_counter++].command = args[1];
-
+            execvp(args[1], &args[1]);
+            exit(1);
           }
           else
           {
-            printf("\nSoryy, you can not run more than 5 jobs on the background..");
-            //run the 6th on the fg
-            waitpid(pid, NULL, 0);
+            execvp(args[0], args);
+            printf( "Child process could not do execvp\n");
+            exit(1);
           }
-          // printf("[BG proc %d started]\n", pid);
-
-          if (WIFEXITED(status)) {
-            printf("exited, status=%d\n", WEXITSTATUS(status));
-          } else if (WIFSIGNALED(status)) {
-            printf("killed by signal %d\n", WTERMSIG(status));
-          } else if (WIFSTOPPED(status)) {
-            printf("stopped by signal %d\n", WSTOPSIG(status));
-          } else if (WIFCONTINUED(status)) {
-            printf("continued\n");
+        } else 
+        {                    //Parent
+          if(strcmp(args[0], "bg")==0)
+          {
+            //check the limit of gb jobs [5]
+            if(bgj_counter < MAX_JOBS_NUMBER)
+            {
+              //allocate when initialize
+              jobs_list[bgj_counter].command = calloc(100, sizeof(char*));
+              //find an empty slot in the array
+              //jobs_list[bgj_counter].terminated=0;
+              jobs_list[bgj_counter].pid = ch_pid;
+              strcpy(jobs_list[bgj_counter].command, args[1]);
+              bgj_counter++;
+            } else
+            {
+              printf("\nSoryy, you can not run more than 5 jobs on the background..");
+              //run the 6th on the fg
+              waitpid(pid, NULL, 0);
+            }
+            // printf("[BG proc %d started]\n", pid);
+            if (WIFEXITED(status)) {
+              printf("exited, status=%d\n", WEXITSTATUS(status));
+            } else if (WIFSIGNALED(status)) {
+              printf("killed by signal %d\n", WTERMSIG(status));
+            } else if (WIFSTOPPED(status)) {
+              printf("stopped by signal %d\n", WSTOPSIG(status));
+            } else if (WIFCONTINUED(status)) {
+              printf("continued\n");
+            }
+          } else
+          {
+            printf("[proc %d started]\n", pid);
+            waitpid(ch_pid, NULL, 0);
           }
         }
+      } else //ch_pid<0
+      {
+        printf( "Forking failed...\n");
       }
     }
 
