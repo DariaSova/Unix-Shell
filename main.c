3cfacd9ed659f6a0d93e3d0e3bba6bab98c13bcb
Daria Sova <todariasova@gmail.com>
2016-05-03 12:56:17 -0700

    Add parent_process.h

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #include "constants.h"
 #include "bg_job.h"
 #include "common.h"
+#include "parent_process.h"
 
 int main ( void )
 {
@@ -94,35 +95,9 @@ int main ( void )
             exit(1);
           }
         } else 
-        {                    //Parent
-          if(strcmp(args[0], "bg")==0)
-          {
-            //check the limit of gb jobs [5]
-            if(bgj_counter < MAX_JOBS_NUMBER)
-            {
-              int i=0;
-              while(jobs_list[i].command!=NULL&&i<MAX_JOBS_NUMBER)
-              {
-                i++;
-              }
-              //allocate when initialize
-              jobs_list[i].command = calloc(100, sizeof(char*));
-              //find an empty slot in the array
-              jobs_list[i].pid = ch_pid;
-              jobs_list[i].status = "R";
-              strcpy(jobs_list[i].command, args[1]);
-              bgj_counter++;
-            } else
-            {
-              printf("\nSorry, you can not run more than 5 jobs on the background..\n");
-              //kill the 6th job
-              kill(ch_pid, SIGSTOP);
-            }
-          } else
-          {
-            printf("Process with pid %d started\n", pid);
-            waitpid(ch_pid, NULL, 0);
-          }
+        {
+          //Parent
+          bgj_counter = parent_job(jobs_list, bgj_counter, pid, ch_pid, args);
         }
       } else //ch_pid<0
       {
